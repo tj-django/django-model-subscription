736f38e4c1b15a0032e8714859dbd0c2f15f158e
Tonye Jack <tonye@coherentpath.com>
2019-11-03 13:03:37 -0500

    Updated the path.

--- setup.py
+++ setup.py
@@ -10,13 +10,17 @@ https://github.com/orsinium/poetry-setup
 # ----------------------------------------------------------------
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
-from os import path
+from os import path, chdir
 # io.open is needed for projects that support Python 2.7
 # It ensures open() defaults to text mode with universal newlines,
 # and accepts an argument to specify the text encoding
 # Python 3 only projects can skip this import
 from io import open
-here = path.abspath(path.dirname(__file__))
+
+# allow setup.py to be run from any path
+chdir(path.normpath(path.join(path.abspath(__file__), path.pardir)))
+
+here = path.dirname(__file__)
 # Get the long description from the README file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
