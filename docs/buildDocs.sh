f21027fb273dd45356dbe66254f0affa7af187db
Tonye Jack <jtonye@ymail.com>
2021-03-21 15:22:05 -0400

    Update buildDocs.sh

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -16,7 +16,7 @@
 ###################
 
 apt-get update
-apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme python3-recommonmark
+apt-get -y install git rsync python3-pip python3-sphinx python3-sphinx-rtd-theme python3-recommonmark
 make install
 
 #####################
