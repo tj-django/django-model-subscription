58018d24dd3378cf8763c6d9032c0d659db82038
Tonye Jack <jtonye@ymail.com>
2021-04-26 11:24:28 -0400

    Update models.py

--- model_subscription/models.py
+++ model_subscription/models.py
@@ -8,19 +8,10 @@ class SubscriptionQuerySet(models.QuerySet):  # type: ignore
     def bulk_create(self, *args, **kwargs):
         objs = super(SubscriptionQuerySet, self).bulk_create(*args, **kwargs)
         connection = connections[self.db]
-        can_return_rows_from_bulk_insert = getattr(
-            connection.features,
-            "can_return_ids_from_bulk_insert",
-            getattr(
-                connection.features,
-                "can_return_rows_from_bulk_insert",
-                False,
-            ),
-        )
         can_notify_bulk_create_subscribers = getattr(
             settings,
             "NOTIFY_BULK_CREATE_SUBSCRIBERS_WITHOUT_PKS",
-            can_return_rows_from_bulk_insert,
+            can_return_rows_from_bulk_insert(connection),
         )
         if can_notify_bulk_create_subscribers:
             self.model.notify_bulk_create(objs)
